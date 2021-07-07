ee658e5577faefe66213106dafe0696e9858ca0a
HyojinKim <gywls00100@gmail.com>
2021-07-07 23:55:46 +0900

    Use API

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "36cd00380cc5515dddcb"; }
+/******/ 		__webpack_require__.h = function() { return "5501d80c67c04b9a9635"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Layout from "../components/Layout";
 import Link from "next/link";
+import fetch from "isomorphic-unfetch";
 
 const ProfileLink = (props) => (
   <div>
@@ -11,11 +12,24 @@ const ProfileLink = (props) => (
 
 const Index = () => (
   <Layout>
-    <h1>Friends List</h1>
+    <h1>Friends List {props.profiles[0]}</h1>
+    {props.profiles.map((profile, index) => (
+      <ProfileLink key={index} profile={profile} />
+    ))}
+
     <ProfileLink profile="hyojin" />
     <ProfileLink profile="sangjo" />
     <ProfileLink profile="chunsik" />
   </Layout>
 );
+
+Index.getInitialProps = async function () {
+  const res = await fetch("https://uinames.com/api/?amount=10");
+  const data = await res.json();
+
+  return {
+    profiles: data.map((profile) => profile.name),
+  };
+};
 
 export default Index;
